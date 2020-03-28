d181704fec236a3c9926d3c6c0de5af637a1af1f
Edem Dumenu <ehdemdume@gmail.com>
2020-03-28 15:28:55 -0400

    Created the models for the api

--- scholar4u_project/settings.py
+++ scholar4u_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'scholar4u_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'scholar4u',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
